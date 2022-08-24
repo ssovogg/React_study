4e8c811d68d9d8cb228a31f1d3fd9e77d1f04ff8
ssovogg <susuco77@gmail.com>
2022-08-24 16:05:31 +0900

    login form setting

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Auth = (props) => (
-      <h1>auth</h1>
+const Auth = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const onChange = (event) => {
+    const { target: {name, value} } = event;
+    if(name === "email"){
+      setEmail(value);
+    } else if(name === "password") {
+      setEmail(value);
+    }
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="test" required name="email" placeholder="Email" onChange={onChange} value={email}/>
+        <input type="test" required name="password" placeholder="Password" onChange={onChange} value={password}/>
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </>
   );
+};
 
 export default Auth;
