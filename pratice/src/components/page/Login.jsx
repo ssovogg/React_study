571ebb077671b80070d6465a388bcf31def42fdc
ssovogg <susuco77@gmail.com>
2022-09-07 15:16:36 +0900

    로그인 유효성 검사 추가

--- pratice/src/components/page/Login.jsx
+++ pratice/src/components/page/Login.jsx
@@ -6,57 +6,102 @@ const Login = ({ auth }) => {
   const [createAccount, setCreateAccount] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const toogleAcount = () => setCreateAccount((prev) => !prev);
+  const [emailIsTouched, setEmailIsTouched] = useState(false);
+  const [emailIsValid, setEmailIsValid] = useState(false);
+  const [pwIsTouched, setPwIsTouched] = useState(false);
+  const [pwIsValid, setPwIsValid] = useState(false);
+  const hasError = !emailIsValid && emailIsTouched;
+  const pwHasError = !pwIsValid && pwIsTouched;
+
+  let formIsValid = false;
+  if (!hasError && !pwHasError) {
+    formIsValid = true;
+  }
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+    setEmailIsTouched(true);
+    if (e.target.value.includes("@")) {
+      setEmailIsValid(true);
+    } else {
+      setEmailIsValid(false);
+    }
+  };
+  const onPwChange = (e) => {
+    setPassword(e.target.value);
+    setPwIsTouched(true);
+    if (e.target.value.length > 5) {
+      setPwIsValid(true);
+    } else {
+      setPwIsValid(false);
+    }
+  };
+
+  const onEmailBlur = () => {
+    setEmailIsTouched(true);
+    if (email.trim() === "") {
+      setEmailIsValid(false);
+    }
+  };
+  const onPwBlur = () => {
+    setPwIsTouched(true);
+    if (password.length < 6) {
+      setPwIsValid(false);
+    }
+  };
+
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
-      if(createAccount){
+      if (createAccount) {
         await auth.createAccount(email, password);
         setCreateAccount(false);
-      }else{
+      } else {
         await auth.logIn(email, password);
       }
     } catch (error) {
       setError(error.message);
-      console.log(error.message);
+      console.log(error);
     }
   };
-  const onChange = (e) => {
-    const {
-      target: { name, value },
-    } = e;
-    if(name==="email"){
-      setEmail(value)
-    }else if(name==="password"){
-      setPassword(value)
-    }
-  };
+
   const onClick = (e) => {
     auth.socialLogin(e.target.name);
-  }
+  };
   return (
     <div className={classes.login}>
       <h1 className={classes.title}>sovog</h1>
       <div className={classes.login_form}>
         <form onSubmit={onSubmit}>
           <input
+            className={hasError && classes.invalid}
             type="text"
             placeholder="이메일"
             required
-            onChange={onChange}
+            onChange={onEmailChange}
+            onBlur={onEmailBlur}
             name="email"
             value={email}
           />
+          {hasError && (
+            <p className={classes.error}>이메일 형식에 맞게 입력해주세요</p>
+          )}
           <input
+            className={pwHasError && classes.invalid}
             type="password"
             placeholder="비밀번호"
             required
-            onChange={onChange}
+            onChange={onPwChange}
+            onBlur={onPwBlur}
             name="password"
             value={password}
           />
-          <button type="submit">{createAccount ? "회원가입" : "로그인"}</button>
+          {pwHasError && (
+            <p className={classes.error}>6글자 이상 입력해주세요</p>
+          )}
+          <button type="submit" disabled={!formIsValid}>{createAccount ? "회원가입" : "로그인"}</button>
           <i>{error}</i>
         </form>
         <div className={classes.social_login}>
--- pratice/src/index.js
+++ pratice/src/index.js
@@ -5,16 +5,10 @@ import "@fortawesome/fontawesome-free/js/all.js";
 import './index.css';
 import AuthService from "./service/authService";
 import { dbService, firebaseApp, storageService } from "./service/fbase";
-import ImageUploader from "./service/image_uploader";
 
 const db = dbService;
 const storage = storageService;
 const auth = new AuthService(firebaseApp);
-const imageUploader = new ImageUploader();
-
-const ImgInput = props => {
-  <
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App auth={auth} db={db} storage={storage} />);
